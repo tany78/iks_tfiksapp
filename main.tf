969ac6c232181780a5e2e14eb919870f0ff9d6df
tany78 <90527319+tany78@users.noreply.github.com>
2022-04-25 06:58:04 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,40 @@
+#Helm install of sample app on IKS
+data "terraform_remote_state" "iksws" {
+  backend = "remote"
+  config = {
+    organization = "www-rittando-lab"
+    workspaces = {
+      name = var.ikswsname
+    }
+  }
+}
+
+variable "ikswsname" {
+  type = string
+}
+
+resource helm_release helloiksfrtfcb {
+  name       = "helloiksapp"
+  namespace = "default"
+  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.1.tgz"
+  timeout = 600
+
+  set {
+    name  = "MESSAGE"
+    value = "Hello IKS from TFCB!!"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+  }
+}
+
+locals {
+  kube_config = yamldecode(data.terraform_remote_state.iksws.outputs.kube_config)
+}
+
